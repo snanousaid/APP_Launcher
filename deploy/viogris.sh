06379afd8ab66d1c6e6df57be18fe97ac2b8ec44
snanousaid <s.snanou@nextronic.io>
2026-06-12 23:16:38 +0100

    viogris: pm2 start/stop ACL dans le bash (trap EXIT)

--- deploy/viogris.sh
+++ deploy/viogris.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 export DISPLAY=:0
+
+# Process pm2 ACL dont viogris a besoin : démarre avec viogris,
+# et s'arrête quand viogris se ferme (trap sur la sortie du script).
+pm2 start ACL
+trap 'pm2 stop ACL' EXIT
+
 ./viogris --no-sandbox --disable-gpu
